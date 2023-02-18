1ae3ee33b74bebe67079005bdb68158b78317faf
Adriel <adriel.fsantos@outlook.com>
2023-02-18 15:06:35 -0300

    Desabilita o logger

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
 const helmet = require('helmet');
-const logger = require('morgan');
+//const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -28,7 +28,7 @@ async function main() {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
